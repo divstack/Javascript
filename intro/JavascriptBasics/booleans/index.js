ff5b46498a9b5b9c069cfca0022f17bfd9fe2c8c
samuelhenga <divstack03@gmail.com>
2021-11-21 09:07:00 -0600

    add more content

---
+++ intro/JavascriptBasics/booleans/index.js
@@ -0,0 +1,7 @@
+// verifying that the statement you make is true or false.
+
+//let isStudent = true;
+const students = ['ryan', 'marcus', 'jeff', 'Bryan'];
+students.push('sam')
+//console.log(students);
+console.log(students.includes('sam'));
