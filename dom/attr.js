b45fa48720edc81e8b628c9113108f118c570f62
samuelhenga <divstack03@gmail.com>
2021-12-02 21:56:11 -0600

    add more files

--- dom/attr.js
+++ dom/attr.js
@@ -12,3 +12,14 @@ const para = document.querySelector('.para');
 console.log(para.getAttribute('class'));
 para.setAttribute('class', "newSelector");
 para.setAttribute('style', 'color:green')
+
+
+/* changing style.*/
+const head6 = document.querySelector('.head6');
+head6.setAttribute('style', 'margin: 50px')
+head6.setAttribute('style', 'color: #3ae')
+// head6.setAttribute('style', 'fontSize: 25px')
+head6.style.fontSize ="22px";
+
+const err = document.querySelector('errp');
+err.classList
--- dom/index.js
+++ dom/index.js
@@ -1,57 +1,104 @@
 // using queryselector
-const h = document.querySelector('h1');
+const h = document.querySelector("h1");
 console.log(h);
 
-
-const ps = document.querySelector('p');
+const ps = document.querySelector("p");
 //* replacing existing value.*/
 //ps.innerText = 'Hello';
 
 /*appending a new value*/
 //ps.innerText += 'Hello';
 
-
-const id = document.querySelector('#text')
+const id = document.querySelector("#text");
 
 /* replacing existing value.*/
 //id.innerHTML = '<h4>Id update</h4>'
 
 /*appending a new value*/
-id.innerHTML += '<span> Id update</span>'
+id.innerHTML += "<span> Id update</span>";
 
+const content = document.querySelector(".content");
 
-const content = document.querySelector('.content');
+const people = ["Ryan", "Luigi", "Marco"];
 
-const people = ['Ryan', 'Luigi', 'Marco'];
-
-people.forEach(p => {
-    content.innerHTML += `<p>${p}</p>`
-})
-
-
-
+people.forEach((p) => {
+  content.innerHTML += `<p>${p}</p>`;
+});
 
 // using queryselectorall
 
 /*  updating the list of P*/
-const p = document.querySelectorAll('p');
-p.forEach(para => {
-    console.log(para.innerText, '<=====list');
-    
-})
+const p = document.querySelectorAll("p");
+p.forEach((para) => {
+  console.log(para.innerText, "<=====list");
+});
 
-
-const li = document.querySelectorAll('li');
+const li = document.querySelectorAll("li");
 console.log(li);
-
-
 
 // get element by id
 const t = document.getElementById("text");
-console.log(t, 'idenfying id');
+console.log(t, "idenfying id");
 //t.style.color = 'red'
 
+/*relation parent child*/
+
+const article = document.querySelector("article");
+
+console.log(article.children, "<=== children");
+console.log(Array.from(article.children), "<=== array children");
+console.log(article.children, "<=== children");
+
+Array.from(article.children).forEach((child) => {
+//child.classList.add("article-element");
+  console.log(child, "<==== children");
+});
 
 
+const title = document.querySelector('.head_parent');
+console.log(title , '<=== tit');
+console.log(title.parentElement, 'parentelement');
+console.log(title.parentElement.parentElement, 'parentelement parentelement');
+console.log(title.nextElementSibling , "<=== next sibling");
+console.log(title.previousElementSibling, "previous sibling");
+
+// chaining 
+console.log(title.nextElementSibling.parentElement.children, 'chaining');
+
+
+// working with events
+const btn = document.querySelector('button');
+btn.addEventListener('click' , () => {
+    let name = 'Ryan';
+    console.log('my name is ' + name);
+    title.style.background = 'blue';
+   
+});
+
+const body = document.querySelector('body');
+btn.addEventListener('click' , () => {
+    body.style.background = 'green';
+})
+
+const btnReset = document.querySelector('.btnReset');
+btnReset.addEventListener('click', () => {
+    body.style.background = 'white'
+})
+
+
+const items = document.querySelectorAll('.li_evt');
+items.forEach(item => {
+
+    const name = 'Ryan';
+    item.addEventListener('click', (e)=> {
+     // console.log(e, 'item clicked');
+         //console.log(e.target);
+       // console.log('my name is' + name);
+        // console.log(item, 'event list');
+       // e.target.style.textDecoration = 'line-through';
+        e.target.style.fontWeight = 'bold';
+        e.target.style.fontSize = '20px';
+    })
+})
 
 
